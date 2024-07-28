780b5fd0636eb184bae2e4d2c8e15ac89bbd1dcc
Constantin Piber <cp.piber@gmail.com>
2024-07-28 22:29:32 +0200

    Fix browser typings
    The extended API has never been updated

--- bundle/index.d.ts
+++ bundle/index.d.ts
@@ -2,5 +2,5 @@
 import { Content, Options } from '../dist/lib';
 export * from '../dist/lib';
 
-declare const epub: (options: Options, content: Content) => Promise<Blob>;
+declare const epub: (optionsOrTitle: Options | string, content: Content, ...args: (boolean | number)[]) => Promise<Blob>;
 export default epub;
