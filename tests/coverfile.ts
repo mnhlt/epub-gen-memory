c8de302a05146c8d785a2f1db0ba63a8d8e2a119
Constantin Piber <cp.piber@gmail.com>
2024-07-28 23:59:20 +0200

    Put description in epub v3

--- tests/coverfile.ts
+++ tests/coverfile.ts
@@ -5,6 +5,6 @@ import epub from '../lib';
 (async () => {
   const f = await readFile(resolve(__dirname, '../../demo_preview.png'));
   const file = new File([f], 'cover.png');
-  const content = await epub({ title: 'EPub Gen', cover: file }, [{ content: `<p>Generate EPUB books from HTML with a simple API in Node.js or the browser.</p>` }]);
+  const content = await epub({ title: 'EPub Gen', cover: file, description: 'Epub with cover via <code>File</code> (<i>test</i>)' }, [{ content: `<p>Generate EPUB books from HTML with a simple API in Node.js or the browser.</p>` }]);
   await writeFile(`${__filename.slice(0, -3)}.epub`, Buffer.from(content));
 })();
